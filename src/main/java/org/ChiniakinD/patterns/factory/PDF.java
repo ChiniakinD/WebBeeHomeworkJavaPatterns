2ef351da3fdf2ac5c0e1204db3193f1fa6106500
ChiniakinD <pilotuiga@gmail.com>
2024-06-11 18:23:13 +0300

    docs(factory): Добавлены JavaDoc.
     В интерфейс Document, перечисление DocumentType, классы: DocumentFactory,PDF, Text, Word; интерфейс Document, перечисление DocumentType добавлены JavaDoc.

--- src/main/java/org/ChiniakinD/patterns/factory/PDF.java
+++ src/main/java/org/ChiniakinD/patterns/factory/PDF.java
@@ -2,9 +2,11 @@ package org.ChiniakinD.patterns.factory;
 
 public class PDF implements Document {
 
+    /**
+     * @return строку "Отправлен документ формата pdf"
+     */
     @Override
     public String send() {
-
         return "Отправлен документ формата pdf";
     }
 
